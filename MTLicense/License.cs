0633e5a2843f529e555b44c4eed8744f1d8116b2
YitFei <yifei0309@gmail.com>
2023-03-30 23:07:24 +0800

    Initial commit

---
+++ MTLicense/License.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MTLicense
+{
+    public class License
+    {
+        private static string secretKey = "";
+        //AesEncryption
+        private static byte[] DeriveKey(string password, int keySize)
+        {
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }, 1000))
+            {
+                return deriveBytes.GetBytes(keySize / 8);
+            }
+        }
+
+        public static string EncryptString(string plainText)
+        {
+            byte[] key = DeriveKey(secretKey, 256);
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string DecryptString(string cipherText)
+        {
+            byte[] key = DeriveKey(secretKey, 256);
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ MTLicense/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalString = "Hello World!";
+
+            // 加密
+            string encryptedString = License.EncryptString(originalString);
+            Console.WriteLine("Original String: {0}\nEncrypted String: {1}", originalString, encryptedString);
+
+            // 解密
+            string decryptedString = License.DecryptString(encryptedString);
+            Console.WriteLine("Decrypted String: {0}", decryptedString);
+            Console.ReadKey();
+        }
+    }
+}
